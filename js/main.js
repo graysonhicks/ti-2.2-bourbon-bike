f3a014d55f1ca39771bd85cd3023e9bbd1601a12
Grayson Hicks <graysonhicks@gmail.com>
2016-02-10 16:48:44 -0500

    added carousel, working on responsive positioning

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
 
 $(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
+  $('.responsive').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 569,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 });
